ccd0f036f3a906c079d4816dd00ad1eef0ff00d2
주영 <juyoung@juyeong-ui-Macmini.local>
2026-04-30 16:30:41 +0900

    refactor: 코드 구조 개선

--- js/load-header.js
+++ js/load-header.js
@@ -1,17 +1,16 @@
-
 // load-header.js
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('/portfolio//sub_header.html')
-    .then(res => {
-      if (!res.ok) throw new Error('헤더를 불러오지 못했습니다.');
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/portfolio/sub_header.html")
+    .then((res) => {
+      if (!res.ok) throw new Error("헤더를 불러오지 못했습니다.");
       return res.text();
     })
-    .then(data => {
-      const headerContainer = document.getElementById('header');
+    .then((data) => {
+      const headerContainer = document.getElementById("header");
       if (headerContainer) {
         headerContainer.innerHTML = data;
         initDarkMode();
       }
     })
-    .catch(err => console.error('헤더 로딩 오류:', err));
+    .catch((err) => console.error("헤더 로딩 오류:", err));
 });
